630668544b0ddf970c421a5d728ac0d636564bc0
Thiha Htet Zaw <thihahtetzaw7991@gmail.com>
2024-07-08 17:30:46 +0630

    Add Logout and Create Job Post

--- resources/views/components/employer-logo.blade.php
+++ resources/views/components/employer-logo.blade.php
@@ -1,3 +1,4 @@
-@props(['width' => 90, 'height' => 90])
+@props(['employer','width' => 90, 'height' => 90])
 
-<img src="https://loremflickr.com/{{ $width }}/{{ $height }}/tokyo?random={{ rand(0, 100) }}" alt="" class="rounded-xl">
+{{--<img src="https://loremflickr.com/{{ $width }}/{{ $height }}/tokyo?random={{ rand(0, 100) }}" alt="" class="rounded-xl">--}}
+<img src="{{ asset($employer->logo) }}" width="{{$width}}" alt="" class="rounded-xl">
--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,15 @@
 <?php
 
+use App\Http\Controllers\JobController;
 use App\Http\Controllers\RegisteredUserController;
 use App\Http\Controllers\SearchController;
 use App\Http\Controllers\SessionController;
 use App\Http\Controllers\TagController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [\App\Http\Controllers\JobController::class, 'index']);
-Route::get('/jobs/create', [\App\Http\Controllers\JobController::class, 'create'])->middleware('auth');
-Route::post('/jobs', [\App\Http\Controllers\JobController::class, 'store'])->middleware('auth');
+Route::get('/', [JobController::class, 'index']);
+Route::get('/jobs/create', [JobController::class, 'create'])->middleware('auth');
+Route::post('/jobs', [JobController::class, 'store'])->middleware('auth');
 
 Route::get('/search', SearchController::class);
 Route::get('/tags/{tag:name}', TagController::class);
@@ -20,4 +21,4 @@ Route::middleware('guest')->group(function () {
     Route::post('/login',[SessionController::class, 'store']);
 });
 
-Route::delete('/login',[SessionController::class, 'destroy'])->middleware('auth');
+Route::delete('/logout', [SessionController::class, 'destroy'])->middleware('auth');
